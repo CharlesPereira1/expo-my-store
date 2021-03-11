5f5702ba9f814fa7d278600089f70299ae2f4eb3
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-03-11 00:25:48 -0300

    styles and functions home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/banner.png")}
+          style={styles.image}
+        />
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  header: {
+    marginBottom: 8,
+  },
+  image: {},
+});
 
 export default Home;
