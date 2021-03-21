0500b35d0ad6c1ad4fd7a8d0dde2ba26f9204ac8
charlespereira1 <charlesti.ptu@gmail.com>
2021-03-21 20:43:10 -0300

    config page shoes components

--- src/components/Shoes/index.tsx
+++ src/components/Shoes/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
 
-import styles from "./styles";
+import img from "../../assets/1.png";
 
 const Shoes: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Shoes</Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <Image source={img} style={styles.shoesImage} />
+      <Text style={styles.shoesText}>Nike Shox 10</Text>
+      <Text style={styles.shoesText}>R$ 190,99</Text>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+  shoesImage: {},
+  shoesText: {},
+});
 
 export default Shoes;
