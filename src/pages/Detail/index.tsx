b029a924d91415a43dd9351b7cac9c4cfe3ea4c0
charlespereira1 <charlesti.ptu@gmail.com>
2021-03-30 23:51:54 -0300

    function page detail component

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+
+import detail from "../../assets/detail.png";
 
 import styles from "./styles";
 
 const Detail: React.FC = () => {
-  return <View />;
+  const navigate = useNavigation();
+
+  navigate.setOptions({
+    headerTitle: "Nike Downshifter 10",
+  });
+
+  return (
+    <View>
+      <Image source={detail}></Image>
+    </View>
+  );
 };
 
 export default Detail;
