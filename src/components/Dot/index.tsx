19218c28a9fb176ea658fc09f84cdd02a1a4221e
charlespereira1 <charlesti.ptu@gmail.com>
2021-04-02 00:02:38 -0300

    function page detail component

---
+++ src/components/Dot/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Dot: React.FC = () => {
+  return (
+    <Container>
+      <h1>Dot</h1>
+    </Container>
+  );
+};
+
+export default Dot;
