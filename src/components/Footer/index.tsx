708b7168c02aeabc5f0439ad1f6c7f5df87dd594
charlespereira1 <charlesti.ptu@gmail.com>
2021-04-13 23:52:15 -0300

    function component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+// interface FooterProps {
+//   children: ReactNode;
+// }
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <h1>Footer</h1>
+    </Container>
+  );
+};
+
+export default Footer;
